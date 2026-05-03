561c2c19f02443d9d876765f98b9f42c4f6b24c6
qqnoodle <foongqiming@gmail.com>
2026-05-03 10:30:06 +0800

    minor attempt at creating search bar

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,18 +1,46 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { StyleSheet, View, Text, Pressable, TextInput } from 'react-native';
 import OptionSelector from '../../components/OptionSelector';
 export default function App() {
     //state to manage optionSelector
     const [selected, updateSelected] = useState(0);
     return (
         <View style={{ backgroundColor: 'white' }}>
-            <Text> Route Options </Text>
-            <OptionSelector
-                options={['Fastest', 'Sheltered', 'Accessible']}
-                selected={selected}
-                onSelect={updateSelected}
-            />
-        </View >
+            <View>
+                <Text> Route Options </Text>
+                <OptionSelector
+                    options={['Fastest', 'Sheltered', 'Accessible']}
+                    selected={selected}
+                    onSelect={updateSelected}
+                />
+            </View >
+            <View style={{ flexDirection: 'row' }}>
+                <Text> Start </Text>
+                <TextInput
+                    placeholder='Search'
+                    clearButtonMode='always'
+                    autoCapitalize='none'
+                    autoCorrect={false}
+                    style={styles.searchbar}
+                />
+            </View>
+        </View>
     );
 }
+
+const styles = StyleSheet.create(
+    {
+        searchbar: {
+            marginHorizontal: 20,
+            marginVertical: 10,
+            paddingHorizontal: 20,
+            paddingVertical: 10,
+            borderColor: '#c4c8cf',
+            borderRadius: 8,
+            borderWidth: 1,
+            flex: 1,
+
+        },
+    }
+);
 
