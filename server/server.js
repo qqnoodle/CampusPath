e59dde3e746f40179a7c5f1eaf18dce1e73dde3d
qqnoodle <foongqiming@gmail.com>
2026-05-03 11:18:03 +0800

    inital set up

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
 app.use(express.json());
-
 app.get("/", (req, res) => {
     res.send("API is running...");
 });
 
-const PORT = 5000;
-app.listen(PORT, () => {
-    console.log("Server is running on ${PORT}");
-});
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
 
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on ${process.env.PORT}`);
+});
