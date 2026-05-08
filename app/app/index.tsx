9eb9735f8cb99117b5b963f3d80045ef7f8f5f8e
qqnoodle <foongqiming@gmail.com>
2026-05-08 16:09:36 +0800

    modified index for testing purpose

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,12 +1,43 @@
 import React, { useState, useEffect } from 'react'
 import { StyleSheet, View, Text, Pressable, TextInput } from 'react-native';
-import OptionSelector from '../../components/OptionSelector';
+import OptionSelector from '../components/OptionSelector';
 import LocationSearch from "../components/LocationSearch";
+import LocationSearchBar from '../components/LocationSearchBar';
+import { SearchResultItem } from '../types/SearchResultItem';
 
 export default function Index() {
-    return <LocationSearch />;
+    const [startLocation, setStartLocation] = useState<SearchResultItem | null>(null);
+    const [destinationLocation, setDestinationLocation] = useState<SearchResultItem | null>(null);
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}> CampusPath Navigator</Text>
+            <LocationSearchBar
+                mainText="Start Location"
+                defaultSearchText="Search Starting...."
+                setOutput={setStartLocation}
+            />
+            <LocationSearchBar
+                mainText="End Location"
+                defaultSearchText="Search Destination...."
+                setOutput={setDestinationLocation}
+            />
+        </View>
+    );
 }
 
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: "#fff",
+    },
+    title: {
+        fontSize: 28,
+        fontWeight: "bold",
+        marginBottom: 24,
+    },
+});
 /*
 export default function App() {
     //state to manage optionSelector
@@ -35,20 +66,4 @@ export default function App() {
     );
 }
 */
-
-const styles = StyleSheet.create(
-    {
-        searchbar: {
-            marginHorizontal: 20,
-            marginVertical: 10,
-            paddingHorizontal: 20,
-            paddingVertical: 10,
-            borderColor: '#c4c8cf',
-            borderRadius: 8,
-            borderWidth: 1,
-            flex: 1,
-
-        },
-    }
-);
 
