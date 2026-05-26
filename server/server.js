364f1234424d98b125468d13e2cbd9b3e5c8d7da
qqnoodle <foongqiming@gmail.com>
2026-05-26 16:33:27 +0800

    attempt in fixing cors once again

--- server/server.js
+++ server/server.js
@@ -10,12 +10,24 @@ const PORT = process.env.PORT ? process.env.PORT : 3000;
 const locationRoute = require("./routes/location.route.js");
 const pathRoute = require("./routes/path.route");
 
-app.use(cors({
-    origin: "*", // allow everything
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
-app.options("*", cors());
+app.use(cors());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET,POST,PUT,DELETE,OPTIONS"
+    );
+
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
+
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
