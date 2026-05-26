dd3e40b8b09a179d186064d14c20adb67e35adae
qqnoodle <foongqiming@gmail.com>
2026-05-26 15:59:39 +0800

    modified configuration in attempt to fix cors issue

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
