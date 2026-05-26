8ceb86c6fa5fa8efaeed76fe9ad1e9d1f66aed1b
currycurry11 <lhyjoshua04@gmail.com>
2026-05-26 17:41:25 +0800

    added app.listen to server.js

--- server/server.js
+++ server/server.js
@@ -63,4 +63,9 @@ mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.log(err));
 
+// added listen
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`);
+});
+
 module.exports = app;
