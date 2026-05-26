029df1c78176ebb9998ffd9fd84f731f50f8840a
qqnoodle <foongqiming@gmail.com>
2026-05-26 12:35:00 +0800

    more fixes to get API working

--- server/server.js
+++ server/server.js
@@ -8,7 +8,13 @@ const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 3000;
 const pathRoute = require("./routes/path.route");
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization']
+
+    }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
